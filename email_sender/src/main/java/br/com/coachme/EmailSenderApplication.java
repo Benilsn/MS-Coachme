0e12da7d5d5acfec2285d3d85ee51b892159b0d6
bndo <bndo@gft.com>
2022-11-16 10:12:58 -0300

    minor

--- email_sender/src/main/java/br/com/coachme/EmailSenderApplication.java
+++ email_sender/src/main/java/br/com/coachme/EmailSenderApplication.java
@@ -1,10 +1,7 @@
 package br.com.coachme;
 
-import org.hibernate.annotations.Parent;
-import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
